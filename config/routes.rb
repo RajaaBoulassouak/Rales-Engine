a48206f4c85334743615f8000b4674362abdfcdc
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-10-05 16:42:26 -0600

    Adds random functionality to all categories

--- config/routes.rb
+++ config/routes.rb
@@ -9,31 +9,37 @@ Rails.application.routes.draw do
         get '/:id/favorite_customer' => 'favorite_customer#show'
         get '/find' => 'find#show'
         get '/find_all' => 'find#index'
+        get '/random' => 'random#show'
       end 
     
       namespace :invoices do 
         get '/find' => 'find#show'
         get '/find_all' => 'find#index'
+        get '/random' => 'random#show'
       end
       
       namespace :invoice_items do 
         get '/find' => 'find#show'
         get '/find_all' => 'find#index'
+        get '/random' => 'random#show'
       end
       
       namespace :items do 
         get '/find' => 'find#show'
         get '/find_all' => 'find#index'
+        get '/random' => 'random#show'
       end
       
       namespace :customers do 
         get '/find' => 'find#show'
         get '/find_all' => 'find#index'
+        get '/random' => 'random#show'
       end
       
       namespace :transactions do 
         get '/find' => 'find#show'
         get '/find_all' => 'find#index'
+        get '/random' => 'random#show'
       end
       
       resources :merchants, only: [:index, :show] 
