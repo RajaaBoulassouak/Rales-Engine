3aeb940f81dac176b30bc27c6362f3dae4afd098
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-10-07 20:59:07 -0600

    adjusts item best day

--- app/controllers/api/v1/items/best_day_controller.rb
+++ app/controllers/api/v1/items/best_day_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::Items::BestDayController < ApplicationController
   
   def show 
-    render json: Item.best_day(params[:item_id]),
-    serializer: ItemBestDaySerializer
+    render json: Item.best_day(params[:item_id])
   end 
 end
--- app/serializers/item_best_day_serializer.rb
+++
@@ -1,7 +0,0 @@
-class ItemBestDaySerializer < ActiveModel::Serializer
-  attributes :best_day
-
-  def best_day
-    object
-  end
-end
